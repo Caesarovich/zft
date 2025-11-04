8f23f504e487a4710a8c1ce4ad90239dfaad11b9
Caesarovich <cesar@caesarovich.xyz>
2025-11-04 10:16:13 +0100

    Add run script

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # ZFT
 
+This project is a Libft tester written in Zig. I chose Zig because it has great C interoperability and I wanted to learn this language.
+
 ## Installation Instructions
 
 To install ZFT, run the following command in your terminal:
@@ -13,3 +15,5 @@ If you want to reinstall ZFT and overwrite the existing installation, you can us
 ```bash
 curl -sSL https://raw.githubusercontent.com/Caesarovich/zft/refs/heads/main/install.sh | bash -s -- --reinstall
 ```
+
+This will clone the repository in your Downloads folder and create an alias `zft` in your `.bashrc` file for easy access.
---
+++ run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+ZFT_PATH=$(realpath "$(dirname "$0")")
+ZIG_VERSION=0.15.2
+ZIG_ARCH=$(uname -m)
+ZIG_PLATFORM=$(uname | tr '[:upper:]' '[:lower:]')
+ZIG_BIN="$ZFT_PATH/zig-$ZIG_ARCH-$ZIG_PLATFORM-$ZIG_VERSION/zig"
+
+WORK_DIR=$(dirname "$0")
+VALGRIND_FLAG=false
+BONUS_FLAG=false
+
+
+for arg in "$@"; do
+	if [ "$arg" = "--valgrind" ]; then
+		VALGRIND_FLAG=true
+		break
+	fi
+
+	if [ "$arg" = "--bonus" ]; then
+		BONUS_FLAG=true
+		continue
+	fi
+	
+	# If not a flag, assume it's the working directory
+	WORK_DIR="$(realpath "$arg")"
+done
+
+# Build the project
+cd "$ZFT_PATH" || { echo "Failed to change directory to $ZFT_PATH"; exit 1; }
+RELATIVE_WORK_DIR=$(realpath --relative-to="$ZFT_PATH" "$WORK_DIR")
+
+$ZIG_BIN build -Dlibft-path="$RELATIVE_WORK_DIR" -Dbonus="$BONUS_FLAG"
+
+# If build failed, exit
+if [ $? -ne 0 ]; then
+	echo "Build failed."
+	exit 1
+fi
+
+
+# Run the project
+if [ "$VALGRIND_FLAG" = true ]; then
+	valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes "$ZFT_PATH/zig-out/bin/zft"
+else
+	"$ZFT_PATH/zig-out/bin/zft"
+fi
